ef4e0b7d412071eb62269443f6a1724217c37dc9
Patrick <patrick.mayeda@gmail.com>
2023-07-31 10:29:59 -0600

    attempting to deal with merge issues

--- Server/models/Get.js
+++ Server/models/Get.js
@@ -2,7 +2,6 @@ const db = require('../database/db.js');
 const axios = require('axios');
 
 exports.getOne = ({title}) => {
-  console.log(title);
   var queryStr = `select * from games where title = '${title}'`;
   return db.query(queryStr);
 }
@@ -10,8 +9,7 @@ exports.getOne = ({title}) => {
 exports.queryBggForExactResult = (title) => {
   var parsed = title.split(' ');
   parsed = parsed.join('%20')
-  console.log(parsed);
-  queryUrl = 'https://boardgamegeek.com/xmlapi/search?search=Crossbows%20and%20Catapults&exact=1'
+  queryUrl = 'https://boardgamegeek.com/xmlapi/search?search=' + parsed + '&exact=1'
   return axios.get(queryUrl);
 }
 
