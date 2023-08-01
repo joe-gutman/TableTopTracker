0ce9996834fe7a30cdea71f19e8e5aa4a1caa08b
Loren <loren.johnson99@yahoo.com>
2023-07-31 19:15:59 -0700

    saving progress for today

--- TableTopTracker/theme.js
+++ TableTopTracker/theme.js
@@ -1,12 +1,17 @@
 import * as React from 'react';
 import { MD3LightTheme as DefaultTheme, PaperProvider } from 'react-native-paper';
 
+import { useFonts } from 'expo-font'
+
 const theme = {
   ...DefaultTheme,
   colors: {
     ...DefaultTheme.colors,
     primary: 'tomato',
     secondary: 'yellow'
+  },
+  fonts: {
+
   }
 };
 
