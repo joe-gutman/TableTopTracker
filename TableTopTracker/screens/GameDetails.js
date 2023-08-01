79cac6f1aca56c0f69fcac15380e341ecd69f4d3
Loren <loren.johnson99@yahoo.com>
2023-08-01 12:47:08 -0700

    refines information shown in description

--- TableTopTracker/screens/GameDetails.js
+++ TableTopTracker/screens/GameDetails.js
@@ -20,6 +20,8 @@ export default function GameDetails () {
     },
     detailsContainer: {
       flex: 1,
+      backgroundColor: '#FBF5E7',
+      padding: 50,
     },
   });
 
@@ -35,8 +37,13 @@ export default function GameDetails () {
     </View>
     <View style={styles.detailsContainer}>
       <Text>{dummyGame.name}</Text>
-      <Text>{dummyGame.minPlayers}
-      </Text>
+      <Text>
+        {/* render player number or range */}
+        {dummyGame.minPlayers===dummyGame.maxPlayers
+          ? dummyGame.minPlayers
+          : `${dummyGame.minPlayers}-${dummyGame.maxPlayers}`}
+        {/* render 'player' or 'players' */}
+        </Text>
       <Text>{dummyGame.category.join(',')}</Text>
       <Text>{dummyGame.minPlayTime}
         </Text>
@@ -47,19 +54,3 @@ export default function GameDetails () {
     </>
   )
 }
-
-// // DUMMY DATA
-// const gameID = 675;
-// const year = 1977;
-// const minPlayers = 2;
-// const maxPlayers = 2;
-// const playTime = 45;
-// const minPlayTime = 45;
-// const maxPlayTime = 45;
-// const age = 12;
-// const name = "4th Dimension";
-// const secondaryName = "Fourth Dimension";
-// const description = "4th Dimension is a very spacey-looking game with a small circular board divided into sectors each track of which has twice as many spaces as the one closer to the center. Each player get 6 warriors, 3 rangers, 2 guardians, and one time lord. Guardians can capture warriors and rangers; rangers can capture warriors; the time lord can capture rangers and guardians; but the lowly warriors are the only ones who can capture the time lord and win the game. Captures are made by moving next to rather than on top of enemy pieces. Each piece can move only one space at a time but they can also &quot;warp&quot; by leaving the board and leaving a 4-D marker behind. The piece can then stay in the warp zone for up to three turns or warp back onto the board up to two spaces away from the 4-D marker. This game has an inconveniently small gameboard and plastic pieces that look like little futuristic chairs.<br/><br/>The game was first published in Games &amp; Puzzles magazine. The designers produced it as a standalone game later the same year, and it was later licensed by TSR.";
-// const thumbnail = "https://cf.geekdo-images.com/Li9nJ4DOsFs1CuwZjkyg3g__thumb/img/6AUVALlhQ_dtP04JEAJXG1gQKhY=/fit-in/200x150/filters:strip_icc()/pic582574.jpg";
-// const image = "https://cf.geekdo-images.com/Li9nJ4DOsFs1CuwZjkyg3g__original/img/29VmqkcRWoq_WS9uGtftdf6teyU=/0x0/filters:format(jpeg)/pic582574.jpg";
-// const category = ["Abstract Category", "Science Fiction"];
