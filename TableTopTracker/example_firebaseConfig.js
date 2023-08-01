4e5224872cd57ba069f212af3d0becca5c1d2b02
darienpoon <darienpoon1@gmail.com>
2023-07-31 18:21:30 -0700

    cleaning up merge requests

---
+++ TableTopTracker/example_firebaseConfig.js
@@ -0,0 +1,9 @@
+export default {
+  apiKey: "",
+  authDomain: "tabletoptracker-d603c.firebaseapp.com",
+  projectId: "tabletoptracker-d603c",
+  storageBucket: "tabletoptracker-d603c.appspot.com",
+  messagingSenderId: "477015804806",
+  appId: "1:477015804806:web:f13f69fbadd2210b777a07",
+  measurementId: "G-M20LEKC85Y"
+};
