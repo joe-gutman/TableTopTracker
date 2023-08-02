6e36ea07395b1593ef3587805f23ad078f8d3f5c
victoriajquinto <victoriajquinto@gmail.com>
2023-08-02 13:31:48 -0700

    add skeleton for theme.js colors

--- TableTopTracker/theme.js
+++ TableTopTracker/theme.js
@@ -3,17 +3,59 @@ import { MD3LightTheme as DefaultTheme, PaperProvider } from 'react-native-paper
 
 import { useFonts } from 'expo-font';
 
+// const theme = {
+//   ...DefaultTheme,
+//   dark: false,
+//   version: 3,
+//   colors: {
+//     ...DefaultTheme.colors,
+//     primary: 'tomato',
+//     secondary: 'yellow'
+//   }
+
+
+// };
+
 const theme = {
-  ...DefaultTheme,
-  dark: false,
-  version: 3,
   colors: {
-    ...DefaultTheme.colors,
-    primary: 'tomato',
-    secondary: 'yellow'
+    //PRIMARY(Green)---------
+    primary: '#7FB069',
+    onPrimary: '#FFFFFF',
+    // primaryContainer: '',
+    // onPrimaryContainer: ,
+
+    //SECONDARY(Orange)   ---------
+    secondary:  '#CA5232',
+    // onSecondary: ,
+    // secondaryContainer: ,
+    // onSecondaryContainer: ,
+
+    //TERTIARY---------
+    // tertiary: ,
+    // onTertiary: ,
+    // tertiaryContainer: ,
+    // onTertiaryContainer: ,
+
+    //NEUTRAL---------
+    // background: ,
+    // onBackground: ,
+    // surface: ,
+    // onSurface: ,
+
+    //NEUTRAL VARIANT---------
+    // surfaceVariant: ,
+    // onSurfaceVariant: ,
+    // outline: ,
+
+    //ERROR---------
+    // error: ,
+    // onError: ,
+    // errorContainer: ,
+    // onErrorContainer: ,
+
+  },
+  fonts: {
+
   }
-
-
-};
-
+}
 export default theme;
