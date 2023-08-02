1b7cf0bc6984955facd63c966831fcfb16d70a5c
victoriajquinto <victoriajquinto@gmail.com>
2023-08-01 19:26:31 -0700

    superimpose card with game details over bckgrd img

--- TableTopTracker/screens/GameDetails.js
+++ TableTopTracker/screens/GameDetails.js
@@ -1,9 +1,9 @@
 //change name of file to whatever you want :D
-import { StyleSheet, Text, Image, View, TouchableOpacity } from 'react-native';
-
+import { StyleSheet, Image, ImageBackground, View, TouchableOpacity } from 'react-native';
 import NavBar from '../components/NavBar/NavBar.js';
-// import react native paper
+import { Card, Text, BottomNavigation } from 'react-native-paper';
 
+// import react native paper
 // Component is currently rendered to bottom of App main page
 
 // figure out how props are gonna be passed to this component
@@ -14,15 +14,15 @@ const dummyGame = {
   complexity: 4.0,
   gameID: 675,
   year: 1977,
-  minPlayers: 2,
-  maxPlayers: 2,
+  minPlayers: 1,
+  maxPlayers: 1,
   playTime: 45,
   minPlayTime: 45,
   maxPlayTime: 45,
   age: 12,
   name: "4th Dimension",
   secondaryName: "Fourth Dimension",
-  description: "4th Dimension is a very spacey-looking game with a small circular board divided into sectors each track of which has twice as many spaces as the one closer to the center. Each player get 6 warriors, 3 rangers, 2 guardians, and one time lord. Guardians can capture warriors and rangers; rangers can capture warriors; the time lord can capture rangers and guardians; but the lowly warriors are the only ones who can capture the time lord and win the game. Captures are made by moving next to rather than on top of enemy pieces. Each piece can move only one space at a time but they can also &quot;warp&quot; by leaving the board and leaving a 4-D marker behind. The piece can then stay in the warp zone for up to three turns or warp back onto the board up to two spaces away from the 4-D marker. This game has an inconveniently small gameboard and plastic pieces that look like little futuristic chairs.<br/><br/>The game was first published in Games &amp; Puzzles magazine. The designers produced it as a standalone game later the same year, and it was later licensed by TSR.",
+  description: "4th Dimension is a very spacey-looking game with a small circular board divided into sectors each track of which has twice as many spaces as the one closer to the center. Each player get 6 warriors, 3 rangers, 2 guardians, and one time lord. Guardians can capture warriors and rangers; rangers can capture warriors; the time lord can capture rangers and guardians; but the lowly warriors are the only ones who can capture the time lord and win the game. Captures are made by moving next to rather than on top of enemy pieces. Each piece can move only one space at a time but they can also &quot;warp&quot; by leaving the board and leaving a 4-D marker behind. The piece can then stay in the warp zone for up to three turns or warp back onto the board up to two spaces away from the 4-D marker. This game has an inconveniently small gameboard and plastic pieces that look like little futuristic chairs. \nThe game was first published in Games &amp; Puzzles magazine. The designers produced it as a standalone game later the same year, and it was later licensed by TSR.",
   thumbnail: "https://cf.geekdo-images.com/Li9nJ4DOsFs1CuwZjkyg3g__thumb/img/6AUVALlhQ_dtP04JEAJXG1gQKhY=/fit-in/200x150/filters:strip_icc()/pic582574.jpg",
   image: "https://cf.geekdo-images.com/Li9nJ4DOsFs1CuwZjkyg3g__original/img/29VmqkcRWoq_WS9uGtftdf6teyU=/0x0/filters:format(jpeg)/pic582574.jpg",
   category: ["Abstract Category", "Science Fiction"]
@@ -31,61 +31,104 @@ const dummyGame = {
 export default function GameDetails ({navigation, route}) {
   const { user } = route.params;
   const styles = StyleSheet.create({
+    parentContainer: {
+      display: 'flex',
+      flex: 1,
+      position: 'absolute',
+      bottom: '0',
+      alignItems: 'flex-end'
+    },
     imageContainer: {
       flex: 1,
-      backgroundColor: 'blue',
     },
     image: {
-      width: 100,
-      height: 100,
+      height: '100%',
+      width: '100%',
+      resizeMode: 'contain',
     },
     detailsContainer: {
       flex: 1,
+      borderTopLeftRadius:10,
+      borderTopRightRadius: 10,
+      backgroundColor: '#FBF5E7',
+      padding: 50,
     },
   });
 
-  return (
-    <View>
-      <Text>Game Details</Text>
-      {/* <View style={styles.imageContainer}>
+  // CHANGE HEIGHT OF IMAGE CARD
+  const cardStyles = StyleSheet.create({
+    image: {
+      height:'50%',
+      width: '100%',
+    },
+  })
 
-        <Image
-          style= {styles.gameImage}
+  return (
+
+      <ImageBackground
+          style={styles.image}
+          resizeMode="cover"
           source={{
               uri: 'https://cf.geekdo-images.com/Li9nJ4DOsFs1CuwZjkyg3g__original/img/29VmqkcRWoq_WS9uGtftdf6teyU=/0x0/filters:format(jpeg)/pic582574.jpg',
             }}
-          />
+      >
+        <View style={styles.parentContainer}>
+      {/* <View style={styles.imageContainer}>
+
       </View>
       <View style={styles.detailsContainer}>
         <Text>{dummyGame.name}</Text>
-        <Text>{dummyGame.minPlayers}
-        </Text>
-        <Text>{dummyGame.category.join(',')}</Text>
-        <Text>{dummyGame.minPlayTime}
+        <Text>{
+          // render player number or number range
+          (dummyGame.minPlayers===dummyGame.maxPlayers)
+            ? dummyGame.minPlayers
+            : `${dummyGame.minPlayers} - ${dummyGame.maxPlayers}`}
+          {((dummyGame.minPlayers===dummyGame.maxPlayers) && (dummyGame.minPlayers===1))
+            ? ` player`
+            : ` players`
+          } </Text>
+        <Text>Recommended Age: {dummyGame.age}</Text>
+        <Text>{dummyGame.category.join(', ')}</Text>
+        <Text>{(dummyGame.minPlayTime===dummyGame.maxPlayTime) ?
+          (dummyGame.minPlayTime) :
+          `${dummyGame.minPlayTime} - ${dummyGame.maxPlayTime}`} minutes
           </Text>
-        <Text>Complexity</Text>
-        <Text>{dummyGame.year}</Text>
+        <Text>Complexity: </Text>
+        <Text>Released: {dummyGame.year}</Text>
         <Text>{dummyGame.description}</Text>
-        <NavBar navigation={navigation} username={username}/>
+        <NavBar navigation={navigation} user={user}/>
       </View> */}
 
-    </View>
+        <Card>
+          <Card.Title title={dummyGame.name} subtitle={dummyGame.secondaryName} />
+          {/* <Card.Cover
+            style={cardStyles.image}
+            source={{uri: 'https://cf.geekdo-images.com/Li9nJ4DOsFs1CuwZjkyg3g__original/img/29VmqkcRWoq_WS9uGtftdf6teyU=/0x0/filters:format(jpeg)/pic582574.jpg'}}/> */}
+          <Card.Content>
+            <Text>{
+              // render player number or number range
+              (dummyGame.minPlayers===dummyGame.maxPlayers)
+                ? dummyGame.minPlayers
+                : `${dummyGame.minPlayers} - ${dummyGame.maxPlayers}`}
+              {((dummyGame.minPlayers===dummyGame.maxPlayers) && (dummyGame.minPlayers===1))
+                ? ` player`
+                : ` players`
+              } </Text>
+            <Text>Recommended Age: {dummyGame.age}</Text>
+            <Text>{dummyGame.category.join(', ')}</Text>
+            <Text>{(dummyGame.minPlayTime===dummyGame.maxPlayTime) ?
+              (dummyGame.minPlayTime) :
+              `${dummyGame.minPlayTime} - ${dummyGame.maxPlayTime}`} minutes
+              </Text>
+            <Text>Complexity: </Text>
+            <Text>Released: {dummyGame.year}</Text>
+            <Text>{dummyGame.description}</Text>
+          </Card.Content>
+        </Card>
+
+      <NavBar navigation={navigation} user={user}/>
+      </View>
+      </ImageBackground>
 
   )
 }
-
-// // DUMMY DATA
-// const gameID = 675;
-// const year = 1977;
-// const minPlayers = 2;
-// const maxPlayers = 2;
-// const playTime = 45;
-// const minPlayTime = 45;
-// const maxPlayTime = 45;
-// const age = 12;
-// const name = "4th Dimension";
-// const secondaryName = "Fourth Dimension";
-// const description = "4th Dimension is a very spacey-looking game with a small circular board divided into sectors each track of which has twice as many spaces as the one closer to the center. Each player get 6 warriors, 3 rangers, 2 guardians, and one time lord. Guardians can capture warriors and rangers; rangers can capture warriors; the time lord can capture rangers and guardians; but the lowly warriors are the only ones who can capture the time lord and win the game. Captures are made by moving next to rather than on top of enemy pieces. Each piece can move only one space at a time but they can also &quot;warp&quot; by leaving the board and leaving a 4-D marker behind. The piece can then stay in the warp zone for up to three turns or warp back onto the board up to two spaces away from the 4-D marker. This game has an inconveniently small gameboard and plastic pieces that look like little futuristic chairs.<br/><br/>The game was first published in Games &amp; Puzzles magazine. The designers produced it as a standalone game later the same year, and it was later licensed by TSR.";
-// const thumbnail = "https://cf.geekdo-images.com/Li9nJ4DOsFs1CuwZjkyg3g__thumb/img/6AUVALlhQ_dtP04JEAJXG1gQKhY=/fit-in/200x150/filters:strip_icc()/pic582574.jpg";
-// const image = "https://cf.geekdo-images.com/Li9nJ4DOsFs1CuwZjkyg3g__original/img/29VmqkcRWoq_WS9uGtftdf6teyU=/0x0/filters:format(jpeg)/pic582574.jpg";
-// const category = ["Abstract Category", "Science Fiction"];
