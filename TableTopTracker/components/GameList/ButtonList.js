08771a2d05e78107f637a465af3680111269eda5
Alex Yamaguchi <alexbilly2110@gmail.com>
2023-08-03 14:30:58 -0500

    fixed indent

--- TableTopTracker/components/GameList/ButtonList.js
+++ TableTopTracker/components/GameList/ButtonList.js
@@ -9,16 +9,16 @@ export default function ButtonList({ collections, listType, setListType }) {
       <ScrollView horizontal>
         { collections.map((collection, i) => (
           <View style={ styles.buttonMargin } key = { i }>
-              <Button
-                key={ collection }
-                title={ collection }
-                onPress={ () => setListType(collection) }
-                style={[
-                  styles.focusButton,
-                  listType === collection && styles.focusButton,
-                  i !== (collections.length - 1) && styles.buttonMargin
-                ]}
-              />
+            <Button
+              key={ collection }
+              title={ collection }
+              onPress={ () => setListType(collection) }
+              style={[
+                styles.focusButton,
+                listType === collection && styles.focusButton,
+                i !== (collections.length - 1) && styles.buttonMargin
+              ]}
+            />
           </View>
         )) }
       </ScrollView>
