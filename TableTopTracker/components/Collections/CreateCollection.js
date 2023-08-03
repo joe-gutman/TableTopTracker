7bede0d7967df5394ad5c8f074d0f6fb4ad73680
Alex Yamaguchi <alexbilly2110@gmail.com>
2023-08-03 17:19:45 -0500

    Save progress on Create Collection modal

--- TableTopTracker/components/Collections/CreateCollection.js
+++ TableTopTracker/components/Collections/CreateCollection.js
@@ -1,14 +1,93 @@
-import React, {useEffect, useState} from "react";
-import {Alert, View, Pressable, StyleSheet} from 'react-native';
-import {Portal, Modal, Text} from 'react-native-paper';
-import {fetchUserCollections} from "../../util/api";
-import {useSelector} from "react-redux";
+import React, { useState } from 'react';
+import { View, TextInput, Pressable, Text, StyleSheet } from 'react-native';
+import { useDispatch, useSelector } from 'react-redux';
+import { Portal, Modal } from 'react-native-paper';
+import { handleOpenModal } from '../../state/modal/actions';
 
-export default function CreateCollection({gameToAdd, onClose}) {
-  const [collections, setCollections] = useState();
+export default function CreateCollection({ gameToAdd, onClose }) {
+  // const [ collections, setCollections ] = useState();
+  const dispatch = useDispatch();
   const user = useSelector(state => state.app.user);
 
-  console.log(user)
+  const [ collectionName, setCollectionName ] = useState('');
+  const [ isLoading, setIsLoading ] = useState(false);
+  const [ error, setError ] = useState(null);
 
-  return <Text>ADD GAME TO COLLECTION</Text>;
+  // function for simulating addition
+  const addCollectionToDB = async(collectionName, gameToAdd) => {
+    // simulate async API call via timeout
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve({ id: 123, collection_name: collectionName });
+      }, 1000);
+    });
+  };
+
+  const handleCreateCollection = async() => {
+    if (collectionName.trim() === '') {
+      setError('Collection name cannot be empty');
+      return;
+    }
+
+    // assumes no error (since passed return)
+    setError(null);
+    setIsLoading(true); // display load text/animation
+
+    try {
+      // call API function to add collection to DB
+      const newCollection = await addCollectionToDB(
+        collectionName, gameToAdd
+      );
+
+      // redux magic to dispatch action to update state w/ new collection data
+      /*
+      dispatch(
+        handleCreateCollection('CREATE_COLLECTION')
+      );
+      */
+
+      // log added collection for now
+      console.log(`Added collection: ${ newCollection }`);
+
+      // close modal
+      onClose();
+
+    } catch(err) {
+      setError(`Failed to add collection to DB: ${ err.message }`);
+
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
+  return (
+    <View>
+      <TextInput
+        style={ styles.textInput }
+        placeholder='Collection Name'
+        value={ collectionName }
+        onChangeText={ setCollectionName }
+      />
+      { error && (
+        <Text style={ styles.errorText }>{ error }</Text>
+      ) }
+      <Pressable
+        title='Create Collection'
+        onPress={ handleCreateCollection }
+        disabled={ isLoading }
+      />
+      { isLoading && <Text>Loading...</Text> }
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  textInput: {
+    borderBottomWidth: 1,
+    marginBottom: 10
+  },
+  errorText: {
+    color: 'red',
+    marginBottom: 10
+  }
+});
