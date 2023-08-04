d0b75e2ccce0f2c61b2428104fa0c8ee8a929234
victoriajquinto <victoriajquinto@gmail.com>
2023-08-04 12:38:07 -0700

    save styling on nav bar

--- TableTopTracker/components/NavBar/NavBar.js
+++ TableTopTracker/components/NavBar/NavBar.js
@@ -1,11 +1,18 @@
 
 import React, {useState, useEffect} from 'react';
-import { StyleSheet, Button, View, TextInput, SafeAreaView, TouchableHighlight, Pressable, Image } from 'react-native';
+import { StyleSheet, Button, View, TextInput, SafeAreaView, TouchableOpacity, Pressable, Image } from 'react-native';
 
 import styles from './navBarStyles.js';
 
+const icons = {
+  'MonopolyHouse': require('../../assets/Asset-NavBar-MonopolyHouse.png'),
+  'Notch': require('../../assets/Asset-NavBar-Notch.png'),
+  'Search': require('../../assets/Asset-NavBar-MagnifyingGlass.png'),
+  'GameWarden': require('../../assets/Asset-GameWarden-Mascot.png'),
+  'UserAccount': require('../../assets/Asset-NavBar-LifePerson.png')
+};
 
-export default function NavBar({navigation, user}) {
+export default function NavBar({navigation, user, gameWarden}) {
 // export default function NavBar({navigation, user, gameWarden}) {
     if(!user) {
         throw new Error('User not defined in NavBar');
@@ -13,57 +20,60 @@ export default function NavBar({navigation, user}) {
 
     return (
       <View>
-        <TouchableHighlight>
-            <View style = {styles.navBarContainer}>
-              <View style={styles.leftColumn}>
-                <View style={styles.littleButton}>
-                  <Pressable
-                    title="Home"
-                    onPress={() =>
-                        navigation.navigate('Home', user={user})
-                    }>home
-                  </Pressable>
-                </View>
-              </View>
-              <View style={styles.centerColumn}>
-                <View styles={styles.centerTop}>
-                  {gameWarden ?
-                  (<Pressable
-                    title="Game Warden"
-                    style={styles.bigButton}
-                    onPress={() =>
-                      navigation.navigate('Game Warden', user={user})
-                    }>game warden
-                  </Pressable>):
-                  (<Pressable
-                    title="Search"
-                    style={styles.bigButton}
-                    onPress={() =>
-                        navigation.navigate('Search', user={user})
-                    }>search
-                  </Pressable>)}
-                </View>
-
-                <View style={styles.centerMiddle}>
-                  {/* notch image */}
-                </View>
-                <View style={styles.centerBottom}>
-
-                </View>
-              </View>
-              <View style={styles.rightColumn}>
-                <View style={styles.littleButton}>
-                  <Pressable
-                    title="User Account"
-                    style={styles.littleButton}
-                    onPress={() =>
-                        navigation.navigate('User Account', user={user})
-                    }>user account
-                  </Pressable>
-                </View>
-              </View>
+        <TouchableOpacity>
+          <View style={styles.floatingButtonContainer}>
+            {gameWarden ?
+            (<Pressable
+              title="Game Warden"
+              style={styles.bigButton}
+              onPress={() =>
+                navigation.navigate('Game Warden', user={user})
+              }>
+                <Image source={icons['GameWarden']} style={{
+                  height: 50,
+                  width: 50,
+                }}/>
+            </Pressable>):
+            (<Pressable
+              title="Search"
+              style={styles.bigButtonContainer}
+              onPress={() =>
+                  navigation.navigate('Search', user={user})
+              }><Image source={icons['Search']} style={styles.bigButton
+              }/>
+            </Pressable>)}
+          </View>
+          <View style = {styles.bottomRowContainer}>
+            <View style={styles.leftColumn}>
+              <Pressable
+                title="Home"
+                onPress={() =>
+                    navigation.navigate('Home', user={user})
+                }>
+                  <Image source={icons['MonopolyHouse']} style={styles.littleButton}/>
+              </Pressable>
             </View>
-        </TouchableHighlight>
+            <View style={styles.centerColumn}>
+              <View style={styles.centerTop}>
+                <Image source={icons['Notch']} style={styles.notch}/>
+              </View>
+              <View style={styles.centerBottom}></View>
+            </View>
+            <View style={styles.rightColumn}>
+              <Pressable
+                title="User Account"
+                style={styles.littleButton}
+                onPress={() =>
+                    navigation.navigate('User Account', user={user})
+                }>
+                  <Image source={icons['UserAccount']} style={{
+                    height: 50,
+                    width: 50,
+                  }}/>
+              </Pressable>
+            </View>
+          </View>
+        </TouchableOpacity>
       </View>
     )
 }
--- TableTopTracker/components/NavBar/navBarStyles.js
+++ TableTopTracker/components/NavBar/navBarStyles.js
@@ -2,64 +2,81 @@ import { StyleSheet } from 'react-native';
 import theme from '../../theme.js';
 
 const styles = StyleSheet.create({
-  navBarContainer: {
+  floatingButtonContainer: {
+    display: 'flex',
+    height: 50,
+    flexDirection: 'row',
+    width: "100%",
+    justifyContent: 'center',
+    marginBottom: 25,
+  },
+  bottomRowContainer: {
     position: 'fixed',
-    backgroundColor: theme.colors.primary,
     bottom: '0',
     display: 'flex',
     flexDirection: "row",
-    // justifyContent: "space-evenly",
     zIndex: "999",
     width: "100%",
-    borderTopLeftRadius: 50,
-    borderTopRightRadius: 50,
+    height: 75,
+    alignItems: 'flex-end',
+    justifyContent: 'center',
+    borderLeftRightRadius: 25,
+    borderTopRightRadius: 25,
   },
   leftColumn: {
-    backgroundColor: theme.colors.primary,
-    borderTopLeftRadius: 50,
-    flex: 2,
-    paddingTop: 20,
-    paddingLeft: 25,
-    paddingRight: 25,
+    borderTopLeftRadius: 25,
     display: 'flex',
-    flexDirection: 'column'
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: theme.colors.primary,
+    flex: 3,
+    height: 75,
   },
   centerColumn: {
-    flex: 2,
-    display: 'flex',
-    flexDirection: 'column'
+    flex: 1,
+    height: 75,
+    width: 75
   },
   centerTop: {
-
-  },
-  centerMiddle: {
-
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: ' ',
+    justifyContent: 'center',
   },
   centerBottom: {
     backgroundColor: theme.colors.primary,
-
-  },
-  rightColumn: {
-    backgroundColor: theme.colors.primary,
-    borderTopRightRadius: 50,
-    flex: 2,
-    paddingTop: 20,
-    paddingLeft: 25,
-    paddingRight: 25,
     display: 'flex',
     flexDirection: 'column',
+    alignItems: 'flex-end',
+    position: 'fixed',
+    bottom: '0',
+    height: 25,
+  },
+  rightColumn: {
+    borderTopRightRadius: 25,
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: theme.colors.primary,
+    flex: 3,
+    height: 75,
   },
   littleButton: {
     backgroundColor: theme.colors.primary,
-    padding: 10,
-    borderRadius: 5,
+    height: 50,
+    width: 50,
+  },
+  bigButtonContainer: {
+    backgroundColor: theme.colors.onSecondary,
+    borderRadius: 50,
   },
   bigButton: {
-    backgroundColor: theme.colors.onSecondary,
+    borderRadius: 50,
   },
-  buttonText: {
-    color: 'black',
-    fontWeight: 'bold',
+  notch: {
+    width: 75
   }
 });
 
