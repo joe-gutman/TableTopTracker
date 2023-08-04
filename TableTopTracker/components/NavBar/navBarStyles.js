a4d4064d07248d8727463e49171869c38581c34d
Alex Yamaguchi <alexbilly2110@gmail.com>
2023-08-04 16:55:53 -0500

    changed searchButtonContainer color

--- TableTopTracker/components/NavBar/navBarStyles.js
+++ TableTopTracker/components/NavBar/navBarStyles.js
@@ -75,7 +75,7 @@ const styles = StyleSheet.create({
     marginTop: 8,
   },
   searchButtonContainer: {
-    backgroundColor: 'white',
+    backgroundColor: '73A260',
     width: 80,
     height: 80,
     marginLeft:'auto',
