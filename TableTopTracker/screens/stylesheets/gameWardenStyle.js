134f908e517e989966b56a39947c76d6858219e3
darienpoon <darienpoon1@gmail.com>
2023-08-04 16:27:46 -0700

    added styling for gamewarden

--- TableTopTracker/screens/stylesheets/gameWardenStyle.js
+++ TableTopTracker/screens/stylesheets/gameWardenStyle.js
@@ -10,8 +10,8 @@ const styles = StyleSheet.create({
   wood:{
     width: '100%',
     height: '120%',
-    resizeMode: 'cover',
     justifyContent: 'center',
+    resizeMode: 'cover',
   },
 
 });
