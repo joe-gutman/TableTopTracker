e414575f444884f1a87627fa5a1e0f86d72288e5
darienpoon <darienpoon1@gmail.com>
2023-08-04 16:21:49 -0700

    added gamewarden image

--- TableTopTracker/screens/stylesheets/gameWardenStyle.js
+++ TableTopTracker/screens/stylesheets/gameWardenStyle.js
@@ -2,7 +2,18 @@ import { StyleSheet } from 'react-native';
 import theme from '../../theme.js';
 
 const styles = StyleSheet.create({
+  logoImage: {
+    height: 300,
+    width: '100%',
+    resizeMode: 'contain',
+  },
+  wood:{
+    width: '100%',
+    height: '100%',
+    resizeMode: 'cover',
+    justifyContent: 'center',
+  },
 
-}),
+});
 
 export default styles;
